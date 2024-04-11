3a5ed753e86847f0f94de2ac0c56dc87add46104
WingZer0o <mtmulch0191@outlook.com>
2024-04-10 21:58:38 -0400

    adding common project and the datalayer

---
+++ Common/HttpClientSingleton.cs
@@ -0,0 +1,25 @@
+﻿namespace Common
+{
+    public sealed class HttpClientSingleton : HttpClient
+    {
+        private static HttpClientSingleton instance;
+
+        private static object padlock = new object();
+        public static HttpClientSingleton Instance => GetThreadSafeInstance();
+        private static HttpClientSingleton GetThreadSafeInstance()
+        {
+            if (instance == null)
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new HttpClientSingleton();
+                    }
+                }
+            }
+            instance.DefaultRequestHeaders.Clear();
+            return instance;
+        }
+    }
+}
