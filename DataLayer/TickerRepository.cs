1ef385bdf3d19ba352e82f2bcbe1bae0c89a1903
WingZer0o <mtmulch0191@outlook.com>
2024-04-12 13:14:56 -0400

    Making this call async

--- DataLayer/TickerRepository.cs
+++ DataLayer/TickerRepository.cs
@@ -5,14 +5,14 @@ namespace DataLayer
 {
     public static class TickerRepository
     {
-        public static void InsertTicker(string ticker)
+        public static async Task InsertTicker(string ticker)
         {
             using SqlConnection connection = new SqlConnection(Constants.ConnectionStrings.StocksDatabase);
             using SqlCommand command = new SqlCommand("dbo.InsertTicker", connection);
             command.CommandType = System.Data.CommandType.StoredProcedure;
             command.Parameters.AddWithValue("@Ticker", ticker);
             connection.Open();
-            command.ExecuteNonQuery();
+            await command.ExecuteNonQueryAsync();
         }
     }
 }
