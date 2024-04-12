38ff71ceff0b125618c104293e280dd861e07883
WingZer0o <mtmulch0191@outlook.com>
2024-04-12 13:06:35 -0400

    adding simple moving average

---
+++ Common/MovingAverages.cs
@@ -0,0 +1,15 @@
+﻿namespace Common
+{
+    public static class MovingAverages
+    {
+        public static double SimpleMovingAverage(List<string> prices)
+        {
+            double total = 0;
+            foreach (string pr in prices)
+            {
+                total += double.Parse(pr);
+            }
+            return total / prices.Count;
+        }
+    }
+}
