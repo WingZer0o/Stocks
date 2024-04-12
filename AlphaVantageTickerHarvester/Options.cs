d70d475070af30b1aaa71c1b936561b3ade650ee
WingZer0o <mtmulch0191@outlook.com>
2024-04-12 13:14:18 -0400

    adding options to display moving averages for the a ticker

---
+++ AlphaVantageTickerHarvester/Options.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using Newtonsoft.Json;
+using TickerHarvester;
+
+namespace AlphaVantageTickerHarvester
+{
+    public static class Options
+    {
+        public static async Task MovingAverage1050200Day(string ticker)
+        {
+            string QUERY_URL = String.Format("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol={0}&apikey={1}", ticker, Constants.ApiKeys.AlphaVantage);
+            Uri queryUri = new Uri(QUERY_URL);
+
+            using HttpClient client = HttpClientSingleton.Instance;
+            HttpResponseMessage response = client.GetAsync(queryUri).GetAwaiter().GetResult();
+            string data = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            TimeSeriesDaily parsedData = JsonConvert.DeserializeObject<TimeSeriesDaily>(data);
+
+            List<string> days10 = parsedData.TimeSeries.Take(10).ToList().Select(x => x.Value.Close).ToList();
+            double days10MovingAverage = MovingAverages.SimpleMovingAverage(days10);
+            Console.WriteLine(String.Format("The 10 Day SMA for {0} is: {1}", ticker, days10MovingAverage));
+
+            List<string> days50 = parsedData.TimeSeries.Take(50).ToList().Select(x => x.Value.Close).ToList();
+            double days50MovingAverage = MovingAverages.SimpleMovingAverage(days50);
+            Console.WriteLine(String.Format("The 50 Day SMA for {0} is: {1}", ticker, days50MovingAverage));
+
+            List<string> days200 = parsedData.TimeSeries.Take(200).ToList().Select(x => x.Value.Close).ToList();
+            double days200MovingAverage = MovingAverages.SimpleMovingAverage(days200);
+            Console.WriteLine(String.Format("The 200 Day SMA for {0}, is : {1}", ticker, days200MovingAverage));
+        }
+    }
+}
--- AlphaVantageTickerHarvester/Program.cs
+++ AlphaVantageTickerHarvester/Program.cs
@@ -1,32 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Common;
+using AlphaVantageTickerHarvester;
 using DataLayer;
-using Newtonsoft.Json;
-using TickerHarvester;
 
 Console.Write("Enter Ticker To Populate Local SQL Server: ");
 string userTickerInput = Console.ReadLine();
 TickerRepository.InsertTicker(userTickerInput);
 
-string QUERY_URL = String.Format("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol={0}&apikey={1}", userTickerInput, Constants.ApiKeys.AlphaVantage);
-Uri queryUri = new Uri(QUERY_URL);
 
-using HttpClient client = HttpClientSingleton.Instance;
-HttpResponseMessage response = client.GetAsync(queryUri).GetAwaiter().GetResult();
-string data = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-TimeSeriesDaily parsedData = JsonConvert.DeserializeObject<TimeSeriesDaily>(data);
+Console.WriteLine("Please enter an option: ");
+Console.WriteLine("1. 10, 50, 200 Day Moving Averages");
 
-List<string> days10 = parsedData.TimeSeries.Take(10).ToList().Select(x => x.Value.Close).ToList();
-double days10MovingAverage = MovingAverages.SimpleMovingAverage(days10);
-Console.WriteLine(String.Format("The 10 Day SMA for {0} is: {1}", userTickerInput, days10MovingAverage));
+string userOption = Console.ReadLine();
 
-List<string> days50 = parsedData.TimeSeries.Take(50).ToList().Select(x => x.Value.Close).ToList();
-double days50MovingAverage = MovingAverages.SimpleMovingAverage(days50);
-Console.WriteLine(String.Format("The 50 Day SMA for {0} is: {1}", userTickerInput, days50MovingAverage));
-
-List<string> days200 = parsedData.TimeSeries.Take(200).ToList().Select(x => x.Value.Close).ToList();
-double days200MovingAverage = MovingAverages.SimpleMovingAverage(days200);
-Console.WriteLine(String.Format("The 200 Day SMA for {0}, is : {1}", userTickerInput, days200MovingAverage));
+switch(userOption)
+{
+    case "1":
+        await Options.MovingAverage1050200Day(userTickerInput);
+        break;
+}
 
 // populate the time series data for the ticker.
 Console.ReadLine();
