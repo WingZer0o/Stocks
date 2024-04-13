67663c5641413fb8b35886487f5b43b32912756d
WingZer0o <mtmulch0191@outlook.com>
2024-04-13 19:14:51 -0400

    adjustments to questions

--- AlphaVantageTickerHarvester/Program.cs
+++ AlphaVantageTickerHarvester/Program.cs
@@ -2,32 +2,35 @@
 using AlphaVantageTickerHarvester;
 using DataLayer;
 
-Console.Write("Enter Ticker To Populate Local SQL Server: ");
-string userTickerInput = Console.ReadLine();
-TickerRepository.InsertTicker(userTickerInput);
-
-
 Console.WriteLine("Please enter an option: ");
 Console.WriteLine("1. 10, 50, 200 Day Simple Moving Averages (SMA)");
 Console.WriteLine("2. Populate Database with Time Series Data");
 Console.WriteLine("3. Populate Portfolio Tickers with Time Series Data");
 Console.WriteLine("4. Populate Database with Income Statement Data");
-
 string userOption = Console.ReadLine();
 
 switch (userOption)
 {
     case "1":
+        Console.Write("Enter Ticker To Populate Local SQL Server: ");
+        string userTickerInput = Console.ReadLine();
+        TickerRepository.InsertTicker(userTickerInput);
         await Options.MovingAverage1050200Day(userTickerInput);
         break;
     case "2":
-        await Options.PopulateDataWithTimeSeriesData(userTickerInput);
+        Console.Write("Enter Ticker To Populate Local SQL Server: ");
+        string userTickerInput2 = Console.ReadLine();
+        TickerRepository.InsertTicker(userTickerInput2);
+        await Options.PopulateDataWithTimeSeriesData(userTickerInput2);
         break;
     case "3":
         await Options.PopulatePortfolioTickersWithTimeSeriesData();
         break;
     case "4":
-        await Options.PopulateIncomeStatementDataForTicker(userTickerInput);
+        Console.Write("Enter Ticker To Populate Local SQL Server: ");
+        string userTickerInput3 = Console.ReadLine();
+        TickerRepository.InsertTicker(userTickerInput3);
+        await Options.PopulateIncomeStatementDataForTicker(userTickerInput3);
         break;
     default:
         Console.WriteLine("You did not enter a valid option");
